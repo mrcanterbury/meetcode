61ff9361cc0371b32b36c01a4c3b3428b5623bdf
mrcanterbury <mattrcanterbury@gmail.com>
2022-02-03 19:12:01 -0600

    created board urls

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("board/", views.board, name="board"),
+    path("board/<str:pk>", views.board, name="board"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'home.html')
+boards = [
+    {'id': 1, 'name': 'Looking for JavaScript Programmers'},
+    {'id': 2, 'name': 'Seeking Frontend Devs in Austin'},
+    {'id': 3, 'name': 'Any Python Developers?'},
+]
 
-def board(request):
-    return render(request, 'board.html')
+def home(request):
+    context = {'boards': boards}
+    return render(request, 'base/home.html', context)
+
+def board(request, pk):
+    board = None
+    for i in boards:
+        if i['id'] == int(pk):
+            board = i
+    
+    context = {'board': board}
+    return render(request, 'base/board.html', context)
