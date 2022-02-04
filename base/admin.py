7abd45a5de2522e6e6543c310ab119bcc207dcaf
mrcanterbury <mattrcanterbury@gmail.com>
2022-02-03 22:50:54 -0600

    created category model

--- base/admin.py
+++ base/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Board
+from .models import Board, Category, Message
 
 admin.site.register(Board)
+admin.site.register(Category)
+admin.site.register(Message)
--- base/models.py
+++ base/models.py
@@ -1,15 +1,31 @@
-from uuid import uuid4
 from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
 
 class Board(models.Model):
-    # author =
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     city = models.CharField(max_length=30, null=True, blank=True)
     state = models.CharField(max_length=2, null=True, blank=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    # members = 
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.name
+    
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.body[0:30]
