6fea07ddf841aec4c0cfd09a72834da9d772fc9b
mrcanterbury <mattrcanterbury@gmail.com>
2022-02-03 21:12:49 -0600

    modified db model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,18 @@
+from uuid import uuid4
 from django.db import models
 
-# Create your models here.
+class Board(models.Model):
+    # author =
+    # city =
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    # members = 
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid4,
+        editable=True)
+    
+    def __str__(self):
+        return self.name
--- base/views.py
+++ base/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import render
+from .models import Board
 
 boards = [
-    {'id': 1, 'name': 'Looking for JavaScript Programmers'},
-    {'id': 2, 'name': 'Seeking Frontend Devs in Austin'},
-    {'id': 3, 'name': 'Any Python Developers?'},
+    {'id': '1b1e182a-b265-4ebe-bd01-2fd465829c89', 'name': 'Looking for JavaScript Programmers'},
+    {'id': '240cc256-cd75-42f1-ae03-6096446e5583', 'name': 'Seeking Frontend Devs in Austin'},
+    {'id': '69707184-2fd3-4ecf-84b8-b16c06c3aedf', 'name': 'Any Python Developers?'},
 ]
 
 def home(request):
+    boards = Board.objects.all()
     context = {'boards': boards}
     return render(request, 'base/home.html', context)
 
 def board(request, pk):
     board = None
-    for i in boards:
-        if i['id'] == int(pk):
-            board = i
+    for post in boards:
+        if post['id'] == str(pk):
+           board = post
     
     context = {'board': board}
     return render(request, 'base/board.html', context)
